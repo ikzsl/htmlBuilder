1b7c65de2be7ca62a7b8171e40387b94c914f60f
Muslim Djalaludinov <ikzsl@yandex.ru>
2020-10-16 13:03:17 +0300

    Update hexletHtmlBuilder.js

--- hexletHtmlBuilder.js
+++ hexletHtmlBuilder.js
@@ -1,37 +1,27 @@
-/* eslint-disable no-case-declarations */
-// const data = ['html', [
-//   ['head', [
-//     ['title', 'hello, hexlet!'],
-//   ]],
-//   ['body', { class: 'container' }, [
-//     ['h1', { class: 'header' }, 'html builder example'],
-//     ['div', [
-//       ['span', 'span text2'],
-//       ['span', 'span text3'],
-//     ]],
-//   ]],
-// ]];
-
-const data = ['html', [
-  ['head', [
-    ['title', 'hello, hexlet!'],
-  ]],
-  ['body', { class: 'container' }, [
-    ['h1', { class: 'header' }, 'html builder example'],
-    ['div', [
-      ['span'],
-      ['span', { class: 'text', id: 'uniq-key' }],
-    ]],
-  ]],
-]];
-
+const data = [
+  'html',
+  [
+    ['head', [['title', 'hello, hexlet!']]],
+    [
+      'body',
+      { class: 'container' },
+      [
+        ['h1', { class: 'header' }, 'html builder example'],
+        ['div', [['span'], ['span', { class: 'text', id: 'uniq-key' }]]],
+      ],
+    ],
+  ],
+];
 
 const iterAst = (ast) => {
   switch (ast.type) {
     case 'tagsList':
       return `${ast.body.map(iterAst).join('')}`;
     case 'tag':
-      const attrsLine = Object.keys(ast.options).reduce((acc, key) => `${acc} ${key}="${ast.options[key]}"`, '');
+      const attrsLine = Object.keys(ast.options).reduce(
+        (acc, key) => `${acc} ${key}="${ast.options[key]}"`,
+        ''
+      );
       return `<${ast.name}${attrsLine}>${iterAst(ast.body)}</${ast.name}>`;
     default:
       return ast;
@@ -63,17 +53,16 @@ const iter = (data) => {
   const processedBody = body instanceof Array ? iter(body) : body;
 
   return {
-    type: 'tag', name: data[0], body: processedBody, options,
+    type: 'tag',
+    name: data[0],
+    body: processedBody,
+    options,
   };
 };
 
 const build = (data) => {
-  // const result = iter(data);
   return iterAst(iter(data));
 };
 
-console.info('\x1b[31m','<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', "\x1b[0m")
-console.log("TCL: build -> result", iter(data));
-console.info('\x1b[31m','<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', "\x1b[0m");
+console.log('TCL: build -> result', iter(data));
 console.log(build(data));
-console.info('\x1b[31m','<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<', "\x1b[0m");
